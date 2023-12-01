6cd323b8a67a06eb4e5e5216b576b8a75f9d2093
Sergio Larios <jr.slarios@gmail.com>
2023-12-01 04:59:23 -0800

    Add a background to the sign up page. Change the hint and text color for the EditText fields. Switched the layout margin a little bit.

--- DiabetesLogging/app/src/main/java/com/example/diabeteslogging/AddCarbs.java
+++ DiabetesLogging/app/src/main/java/com/example/diabeteslogging/AddCarbs.java
@@ -10,5 +10,6 @@ public class AddCarbs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_carbs);
+
     }
 }
